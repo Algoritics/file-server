6928b2b3fbd46b89f56081e91e144b4c7808cfb3
Dylan Franco <dfranco808@gmail.com>
2015-07-16 12:36:51 -1000

    Testing

--- server.js
+++ server.js
@@ -2,16 +2,15 @@ var express = require('express'),
     multer  = require('multer')
 
 var app = express()
-app.use(multer({ dest: './uploads/'}))
 
 app.get('/', function(req, res){
-  res.send('hello');
+  res.send('hello world');
 });
 
-app.post('/', function(req, res){
+app.post('/',[ multer({ dest: './uploads/'}), function(req, res){
     console.log(req.body) // form fields
     console.log(req.files) // form files
     res.status(204).end()
-});
+}]);
 
 app.listen(3000);
