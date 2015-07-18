ad96122c357c668403a3d51b016633d80c7f7a07
Dylan Franco <dfranco808@gmail.com>
2015-07-17 22:04:05 -1000

    Makin sum progress

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 var express = require('express');
+  multer = require('multer'),
+    send = require('send'),
+    uuid = require('node-uuid'),
+    fs = requie('fs');
 var app = express();
+var fileuuid = "";
+var done = false;
+
+app.use(multer({ dest: './uploads/',
+ rename: function (fieldname, filename) {
+ 	fileuuid = uuid.v4();
+    return fileuuid;
+  },
+onFileUploadStart: function (file) {
+  console.log(file.originalname + ' is starting ...')
+},
+onFileUploadComplete: function (file) {
+  console.log(file.fieldname + ' uploaded to  ' + file.path)
+  done=true;
+}
+}));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendfile("index.html");
+});
+
+app.post('/',function(req,res){
+  if(done==true){
+    console.log(req.files);
+    res.end("File uploaded.");
+  }
 });
 
 var server = app.listen(3000, function () {
